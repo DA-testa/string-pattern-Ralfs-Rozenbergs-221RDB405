ee18165cac299d64aba6e9a0bba1accc0073f28d
Ralfs Rozenbergs 221RDB405 <124904297+Ralfs-Rozenbergs-221RDB405@users.noreply.github.com>
2023-04-16 13:56:25 +0000

    x

--- hash_substring.py
+++ hash_substring.py
@@ -1,19 +1,26 @@
+# 221RDB405 Ralfs Rozenbergs
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    input_type = input()
+    pattern = ""
+    text = ""
+    if "F" in input_type:
+        try:
+            file = open("tests/06","r")
+            lines = file.readlines()
+            pattern = lines[0]
+            text = lines[1]
+            file.close()
+        except Exception:
+            print("File not found")
+    elif "I" in input_type:
+        pattern = input()
+        text = input()
+    else:
+        print("Incorrect format - choose I or F")
+
+    return (pattern.rstrip(), text.rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
